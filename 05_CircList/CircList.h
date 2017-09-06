a712e52db7272d899381f4d6d4c3a3f410050916
salvadiaz <salvadiazpesci@gmail.com>
2017-09-06 09:53:38 -0300

    para clase

--- 05_CircList/CircList.h
+++ 05_CircList/CircList.h
@@ -1,10 +1,64 @@
 #ifndef CIRCLIST_H
 #define CIRCLIST_H
 
+#include "Nodo.h"
+#inlcude <iostream>
 
 class CircList {
+private:
+  Nodo<T> *puntero;
+public:
+  CircList();
+  
+  CircList(const CircList<T> &li);
+  
+  ~CircList();
+  
+  bool esVacia();
+  
+  int getTamanio();
+  
+  void intsertar (T dato);
+  
+  void avanzar();
+  
+  
+  
+  void remover();
 
 };
+
+CircList::CircList(){
+  puntero = NULL;
+}
+
+CircList::CircList(const CircList<T> &li){
+  
+}
+
+void CircList:: insertar (T dato){
+ if (puntero == NULL){
+   puntero = new Nodo(dato);
+   puntero->setNext(puntero);
+ }
+  
+  Nodo *tmp = new Nodo(dato, puntero->getNext());
+  puntero->setNext(tmp);
+  puntero = putero->getNext();
+}
+
+void CircList::avanzar(){
+  if (puntero != NULL){
+  puntero = puntero->getNext();
+  }
+}
+
+void CircList::remover(){
+  Nodo *aux = puntero;
+  
+  while (aux->getNext() != NULL){
+    aux = aux->getNext();
+  
 
 
 #endif //CIRCLIST_H
